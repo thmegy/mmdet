75520acddfac4f8b292f16a6413ead83e89823cf
Théo Megy <theo.megy@logiroad.fr>
2022-02-08 10:09:20 +0100

    fix bug in mmdetection

--- configs/yolov3.py
+++ configs/yolov3.py
@@ -1,4 +1,4 @@
-_base_ = '../mmdetection/configs/yolo/yolov3_d53_fp16_mstrain-608_273e_coco.py'
+_base_ = '../mmdetection/configs/yolo/yolov3_d53_mstrain-608_273e_coco.py'
 
 # model settings
 model = dict(
@@ -23,8 +23,8 @@ dataset_type = 'CocoDataset'
 classes = ('Arrachement_pelade', 'Faiencage', 'Nid_de_poule', 'Transversale', 'Longitudinale', 'Reparation')
 data_root = 'data/'
 data = dict(
-    samples_per_gpu=22,
-    workers_per_gpu=8,
+    samples_per_gpu=10,
+    workers_per_gpu=6,
     train=dict(
         type=dataset_type,
         ann_file=data_root + 'cracks_train.json',
@@ -54,11 +54,11 @@ log_config = dict(
 )
 
 # training parameters
-load_from='checkpoints/yolov3_d53_fp16_mstrain-608_273e_coco_20210517_213542-4bc34944.pth'
-#load_from='outputs/yolov3/latest.pth'
+#load_from='checkpoints/yolov3_d53_fp16_mstrain-608_273e_coco_20210517_213542-4bc34944.pth'
+load_from='checkpoints/yolov3_d53_mstrain-608_273e_coco_20210518_115020-a2c3acb8.pth'
 checkpoint_config = dict(interval=25) # save checkpoint every 10 epochs
 evaluation = dict(interval=10)
 # The original learning rate (LR) is set for 8-GPU training.
 # We divide it by 8 since we only use one GPU.
-optimizer = dict(type='SGD', lr=0.001*44/64, momentum=0.9, weight_decay=0.0005)
+optimizer = dict(type='SGD', lr=0.001*10/64, momentum=0.9, weight_decay=0.0005)
 #runner = dict(type='EpochBasedRunner', max_epochs=75)

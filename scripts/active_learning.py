c06875f6a429f0ebbccfee712ac858b745e4d7d7
Théo Megy <theo.megy@logiroad.fr>
2022-02-21 16:54:36 +0100

    AL: add option to resume training

--- scripts/active_learning.py
+++ scripts/active_learning.py
@@ -20,102 +20,119 @@ def select_random(n_pool, n_sel):
 
 
 def main(args):    
-    config = mmcv.Config.fromfile(args.config)
-    test_cfg = config.model.bbox_head.test_cfg
-    
-    train_set_orig = config.data.train.ann_file
-    img_path = config.data.train.img_prefix
-    train_set_name = train_set_orig.replace('.json', '_AL_init.json')
-    pool_set_name = train_set_orig.replace('.json', '_AL_pool_init.json')
-
-    # split training data into start training set and data pool for active learning
-    if args.do_split or not os.path.isfile(train_set_name):
-        os.system( f'python scripts/split_train_val.py --original {train_set_orig} --train {train_set_name} --val {pool_set_name} --ratio {args.ratio}' )
-
-    # 1st training
-    config.data.train.ann_file = train_set_name
     config_AL = args.config.replace('.py', '_AL.py')
-    workdir = f'{args.work_dir}/init_training/'
-    mmcv.Config.dump(config, config_AL)
-    if args.do_init_train or not os.path.isfile(f'{workdir}/latest.pth'):
-        if args.n_gpu == 1:
-            os.system( f'python mmdetection/tools/train.py {config_AL} --work-dir {workdir}/' )
-        else:
-            os.system( f'./mmdetection/tools/dist_train.sh {config_AL} {args.n_gpu} --work-dir {workdir}/' )
-
-    # create list of pool images
-    with open(pool_set_name, 'rt') as f:
-        pool = json.load(f)
-        pool_img = [os.path.join(img_path, im['file_name']) for im in pool['images']]
-
-    with open(train_set_name, 'rt') as f:
-        train = json.load(f)
+    if args.auto_resume:
+        config = mmcv.Config.fromfile(config_AL)
+    else:
+        config = mmcv.Config.fromfile(args.config)
     
+        train_set_orig = config.data.train.ann_file
+        img_path = config.data.train.img_prefix
+        train_set_name = train_set_orig.replace('.json', '_AL_init.json')
+        pool_set_name = train_set_orig.replace('.json', '_AL_pool_init.json')
+
+        # split training data into start training set and data pool for active learning
+        if args.do_split or not os.path.isfile(train_set_name):
+            os.system( f'python scripts/split_train_val.py --original {train_set_orig} --train {train_set_name} --val {pool_set_name} --ratio {args.ratio}' )
+
+        # 1st training
+        config.data.train.ann_file = train_set_name
+        workdir = f'{args.work_dir}/init_training/'
+        mmcv.Config.dump(config, config_AL)
+        if args.do_init_train or not os.path.isfile(f'{workdir}/latest.pth'):
+            if args.n_gpu == 1:
+                os.system( f'python mmdetection/tools/train.py {config_AL} --work-dir {workdir}/' )
+            else:
+                os.system( f'./mmdetection/tools/dist_train.sh {config_AL} {args.n_gpu} --work-dir {workdir}/' )
+
+        # create list of pool images
+        with open(pool_set_name, 'rt') as f:
+            pool = json.load(f)
+            pool_img = [os.path.join(img_path, im['file_name']) for im in pool['images']]
+
+        with open(train_set_name, 'rt') as f:
+            train = json.load(f)
+
+    test_cfg = config.model.bbox_head.test_cfg
 
     # loop over active learning iterations
-    for ir in range(args.n_round):
+    if args.auto_resume:
+        round_range = range(args.resume_round, args.n_round)
+    else:
+        round_range = range(args.n_round)
+        
+    for ir in round_range:
         print(f'\nRound {ir}\n')
+
+        if not args.auto_resume or ir > args.resume_round:
+            if test_cfg.active_learning.selection_method == 'random':
+                selection = select_random(len(pool_img), test_cfg.active_learning.n_sel)
+            else:
+                # inference over pool images
+                detector = mmdet.apis.init_detector(
+                    config_AL,
+                    f'{workdir}/latest.pth',
+                    device='cuda:0',
+                )
+                
+                # split images into batches to run the inference
+                img_batch_size = 50
+                img_n_batch = len(pool_img) // img_batch_size
+                img_batches = np.array_split( np.array(pool_img), img_n_batch )
+                uncertainty = []
+                print('\nRunning inference on pool set')
+                for img_batch in tqdm.tqdm(img_batches):
+                    uncertainty.append( mmdet.apis.inference_detector(detector, img_batch.tolist(), active_learning=True) )
+                uncertainty = torch.concat(uncertainty)
+
+                torch.cuda.empty_cache()
+                del detector
+                # select images to be added to the training set
+                selection = select_images(test_cfg.active_learning.selection_method, uncertainty, test_cfg.active_learning.n_sel, **test_cfg.active_learning.selection_kwargs)
+
+            # update training set and pool according to selected images
+            sel_id = []
+            for idx in selection.sort(descending=True)[0]:
+                train['images'].append( pool['images'][idx] )
+                sel_id.append( pool['images'][idx]['id'] )
+                
+                pool['images'].remove( pool['images'][idx] )
+                pool_img.remove( pool_img[idx] )
+
+            # Pick annotations corresponding to selected images
+            for annot in pool["annotations"]:
+                if annot["image_id"] in sel_id:
+                    train["annotations"].append(annot)
+                    pool["annotations"].remove(annot)
+
+            # save new train and pool sets
+            with open(pool_set_name.replace('init.json', f'{ir}.json'), "wt") as f_out:
+                json.dump(pool, f_out)
+            with open(train_set_name.replace('init.json', f'{ir}.json'), "wt") as f_out:
+                json.dump(train, f_out)
+
+            # training with updated set
+            config.data.train.ann_file = train_set_name.replace('init.json', f'{ir}.json') # use updated training set
+            if args.do_incremental_learning:
+                config.load_from = f'{workdir}/latest.pth' # checkpoint from previous iteration
+                config.runner['max_epochs'] = args.n_epoch
+            mmcv.Config.dump(config, config_AL)
+
         if test_cfg.active_learning.selection_method == 'random':
-            selection = select_random(len(pool_img), test_cfg.active_learning.n_sel)
+            workdir = f'{args.work_dir}/{test_cfg.active_learning.selection_method}/{test_cfg.active_learning.n_sel}/round_{ir}/'
         else:
-            # inference over pool images
-            detector = mmdet.apis.init_detector(
-                config_AL,
-                f'{workdir}/latest.pth',
-                device='cuda:0',
-            )
-            
-            # split images into batches to run the inference
-            img_batch_size = 50
-            img_n_batch = len(pool_img) // img_batch_size
-            img_batches = np.array_split( np.array(pool_img), img_n_batch )
-            uncertainty = []
-            print('\nRunning inference on pool set')
-            for img_batch in tqdm.tqdm(img_batches):
-                uncertainty.append( mmdet.apis.inference_detector(detector, img_batch.tolist(), active_learning=True) )
-            uncertainty = torch.concat(uncertainty)
+            workdir = f'{args.work_dir}/{test_cfg.active_learning.score_method}/{test_cfg.active_learning.aggregation_method}/{test_cfg.active_learning.selection_method}/{test_cfg.active_learning.n_sel}/round_{ir}/'
 
-            torch.cuda.empty_cache()
-            del detector
-            # select images to be added to the training set
-            selection = select_images(test_cfg.active_learning.selection_method, uncertainty, test_cfg.active_learning.n_sel, **test_cfg.active_learning.selection_kwargs)
-
-        # update training set and pool according to selected images
-        sel_id = []
-        for idx in selection.sort(descending=True)[0]:
-            train['images'].append( pool['images'][idx] )
-            sel_id.append( pool['images'][idx]['id'] )
-
-            pool['images'].remove( pool['images'][idx] )
-            pool_img.remove( pool_img[idx] )
-
-        # Pick annotations corresponding to selected images
-        for annot in pool["annotations"]:
-            if annot["image_id"] in sel_id:
-                train["annotations"].append(annot)
-                pool["annotations"].remove(annot)
-
-        # save new train and pool sets
-        with open(pool_set_name.replace('init.json', f'{ir}.json'), "wt") as f_out:
-            json.dump(pool, f_out)
-        with open(train_set_name.replace('init.json', f'{ir}.json'), "wt") as f_out:
-            json.dump(train, f_out)
-
-        # training with updated set
-        config.data.train.ann_file = train_set_name.replace('init.json', f'{ir}.json') # use updated training set
-        config.load_from = f'{workdir}/latest.pth' # checkpoint from previous iteration
-        config.runner['max_epochs'] = args.n_epoch
-        mmcv.Config.dump(config, config_AL)
-
-        if test_cfg.active_learning.selection_method == 'random':
-            workdir = f'{args.work_dir}/{test_cfg.active_learning.selection_method}/round_{ir}/'
+        if args.auto_resume and ir == args.resume_round:
+            if args.n_gpu == 1:
+                os.system( f'python mmdetection/tools/train.py {config_AL} --work-dir {workdir}/ --auto-resume' )
+            else:
+                os.system( f'./mmdetection/tools/dist_train.sh {config_AL} {args.n_gpu} --work-dir {workdir}/ --auto-resume' )
         else:
-            workdir = f'{args.work_dir}/{test_cfg.active_learning.score_method}/{test_cfg.active_learning.aggregation_method}/{test_cfg.active_learning.selection_method}/round_{ir}/'
-
-        if args.n_gpu == 1:
-            os.system( f'python mmdetection/tools/train.py {config_AL} --work-dir {workdir}/' )
-        else:
-            os.system( f'./mmdetection/tools/dist_train.sh {config_AL} {args.n_gpu} --work-dir {workdir}/' )
+            if args.n_gpu == 1:
+                os.system( f'python mmdetection/tools/train.py {config_AL} --work-dir {workdir}/' )
+            else:
+                os.system( f'./mmdetection/tools/dist_train.sh {config_AL} {args.n_gpu} --work-dir {workdir}/' )
 
         # test
         os.system( f'python mmdetection/tools/test.py {config_AL} {workdir}/latest.pth --work-dir {workdir} --eval bbox' )
@@ -128,13 +145,18 @@ if __name__ == '__main__':
     parser.add_argument('--config', required=True, help='mmdetection config')
     parser.add_argument('--work-dir', required=True, help='output directory')
     parser.add_argument('--n-round', default=10, type=int, help='Number of iterations for active learning')
-    parser.add_argument('--n-epoch', default=5, type=int, help='Number of epochs to update training at each iteration')
     parser.add_argument('--n-gpu', default=1, type=int, help='Number of GPUs to use')
 
     parser.add_argument('--do_split', action='store_true', help='Split original training set into starting training set and pool set')
     parser.add_argument('--ratio', default='0.9', help='The pool will be ratio * N(images) in original training set')
 
     parser.add_argument('--do_init_train', action='store_true', help='Perform initial training')
+
+    parser.add_argument('--do-incremental-learning', action='store_true', help='Do not train from scratch at each round, start from latest checkpoint of previous round')
+    parser.add_argument('--n-epoch', default=5, type=int, help='Number of epochs to update training at each iteration')
+
+    parser.add_argument('--auto-resume', action='store_true', help='Resume training from latest checkpoint of round given in --resume-round')
+    parser.add_argument('--resume-round', default=0, type=int, help='Round to resume from if --auto-resume is used')
 
     args = parser.parse_args()
 

c80fa1a75dcdc0a0bd02d9fb47c94a5261513e5e
Théo Megy <theo.megy@logiroad.fr>
2022-05-06 16:50:34 +0200

    raise score thr in configs/trous_cascade_rcnn.py

--- configs/trous_cascade_rcnn.py
+++ configs/trous_cascade_rcnn.py
@@ -65,7 +65,7 @@ model=dict(
             nms=dict(type='nms', iou_threshold=0.5),
             min_bbox_size=0),
         rcnn=dict(
-            score_thr=0.05,
+            score_thr=0.5,
             nms=dict(type='nms', iou_threshold=0.5),
             max_per_img=500,
             mask_thr_binary=0.6

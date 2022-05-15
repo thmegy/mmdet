e0e5ba950ea13810b0845fcb411ac4ff1b16f28e
Théo Megy <theo.megy@logiroad.fr>
2022-05-15 15:13:14 +0200

    allow several classes for scripts/evaluate.py

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -55,11 +55,11 @@ def draw_text(image, true_positives, false_positives, false_negatives):
         cv.putText(
             image,
             text,
-            (image.shape[1] - 235, y),
+            (image.shape[1] - 1000, y),
             cv.FONT_HERSHEY_SIMPLEX,
-            0.6,
+            3,
             (0, 0, 0),
-            2,
+            3,
             cv.LINE_AA,
         )
 
@@ -67,108 +67,114 @@ def draw_text(image, true_positives, false_positives, false_negatives):
         cv.putText(
             image,
             text,
-            (image.shape[1] - 235, y),
+            (image.shape[1] - 1000, y),
             cv.FONT_HERSHEY_SIMPLEX,
-            0.6,
+            3,
             color,
-            1,
+            3,
             cv.LINE_AA,
         )
 
-    draw_shadow(f"True positives: {true_positives}", image.shape[0] - 60)
-    draw_shadow(f"False positives: {false_positives}", image.shape[0] - 35)
-    draw_shadow(f"False negatives: {false_negatives}", image.shape[0] - 10)
-    draw_color(f"True positives: {true_positives}", image.shape[0] - 60, (0, 255, 0))
-    draw_color(f"False positives: {false_positives}", image.shape[0] - 35, (0, 0, 255))
-    draw_color(f"False negatives: {false_negatives}", image.shape[0] - 10, (255, 0, 0))
+    draw_shadow(f"True positives: {true_positives}", image.shape[0] - 300)
+    draw_shadow(f"False positives: {false_positives}", image.shape[0] - 200)
+    draw_shadow(f"False negatives: {false_negatives}", image.shape[0] - 100)
+    draw_color(f"True positives: {true_positives}", image.shape[0] - 300, (0, 255, 0))
+    draw_color(f"False positives: {false_positives}", image.shape[0] - 200, (0, 0, 255))
+    draw_color(f"False negatives: {false_negatives}", image.shape[0] - 100, (255, 0, 0))
 
 
 if __name__ == "__main__":
     args = parse_arguments()
 
     detector = mmdet.apis.init_detector(args.config, args.checkpoint, device="cuda:0")
-
+    
     with open(args.dataset, "rt") as f_in:
         dataset = json.load(f_in)
 
-    bboxes_per_image = {}
-    for annot in dataset["annotations"]:
-        try:
-            bboxes_per_image[annot["image_id"]].append(annot["bbox"])
-        except KeyError:
-            bboxes_per_image[annot["image_id"]] = []
-            bboxes_per_image[annot["image_id"]].append(annot["bbox"])
+    n_classes = len(dataset['categories'])
+
+    bboxes_per_class = []
+    for ic in range(n_classes):
+        bboxes_per_image = {}
+        for annot in dataset["annotations"]:
+            if annot['category_id'] == ic:
+                try:
+                    bboxes_per_image[annot["image_id"]].append(annot["bbox"])
+                except KeyError:
+                    bboxes_per_image[annot["image_id"]] = []
+                    bboxes_per_image[annot["image_id"]].append(annot["bbox"])
+        bboxes_per_class.append(bboxes_per_image)
 
     all_true_positives = 0
     all_false_positives = 0
     all_false_negatives = 0
     for image_info in tqdm.tqdm(dataset["images"]):
-        try:
-            gt_bboxes = bboxes_per_image[image_info["id"]]
-        except KeyError:
-            # No bbox on that image
-            gt_bboxes = []
 
         image_path = os.path.join(args.images_dir, image_info["file_name"])
-
-        predictions = mmdet.apis.inference_detector(detector, image_path)
-        predictions = predictions[0]
-        predictions = [pred for pred in predictions if pred[4] > args.score_threshold]
-
         image = cv.imread(image_path)
 
-        # Build iou matrix predictions vs gt
-        iou_matrix = np.zeros((len(predictions), len(gt_bboxes)))
-        for i, prediction in enumerate(predictions):
-            for j, gt_bbox in enumerate(gt_bboxes):
-                x1_gt, y1_gt, width_gt, height_gt = gt_bbox
-                x2_gt = x1_gt + width_gt
-                y2_gt = y1_gt + height_gt
+        predictions = mmdet.apis.inference_detector(detector, image_path)
 
-                bbox1 = x1_gt, y1_gt, x2_gt, y2_gt
-                bbox2 = prediction[:4]
-                iou_matrix[i, j] = iou(bbox1, bbox2)
-
-        matches_matrix = iou_matrix > args.iou_threshold
-
-        # Compute and draw results
         true_positives = 0
         false_positives = 0
         false_negatives = 0
-        for i, gt_matches in enumerate(matches_matrix.T):
-            num_matches = gt_matches.sum()
+        for ic, pred in enumerate(predictions): # loop on classes 
+            try:
+                gt_bboxes = bboxes_per_class[ic][image_info["id"]]
+            except KeyError:
+                # No bbox on that image
+                gt_bboxes = []
 
-            if num_matches == 1:
-                true_positives += 1
+            pred = [p for p in pred if p[4] > args.score_threshold]
 
-                # Draw true positives as green
-                pred_idx = gt_matches.argmax()
-                x1, y1, x2, y2 = predictions[pred_idx][:4]
-                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-                cv.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 1)
+            # Build iou matrix predictions vs gt
+            iou_matrix = np.zeros((len(pred), len(gt_bboxes)))
+            for i, p in enumerate(pred):
+                for j, gt_bbox in enumerate(gt_bboxes):
+                    x1_gt, y1_gt, width_gt, height_gt = gt_bbox
+                    x2_gt = x1_gt + width_gt
+                    y2_gt = y1_gt + height_gt
+                    
+                    bbox1 = x1_gt, y1_gt, x2_gt, y2_gt
+                    bbox2 = p[:4]
+                    iou_matrix[i, j] = iou(bbox1, bbox2)
 
-            elif num_matches == 0:
-                false_negatives += 1
+            matches_matrix = iou_matrix > args.iou_threshold
 
-                # Draw false negatives as blue
-                x1, y1, width, height = gt_bboxes[i]
-                x2 = x1 + width
-                y2 = y1 + height
-                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-                cv.rectangle(image, (x1, y1), (x2, y2), (255, 0, 0), 1)
+            # Compute and draw results
+            for i, gt_matches in enumerate(matches_matrix.T):
+                num_matches = gt_matches.sum()
+                if num_matches == 1:
+                    # Draw true positives as green
+                    pred_idx = gt_matches.argmax()
+                    x1, y1, x2, y2 = pred[pred_idx][:4]
+                    x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+                    true_positives += 1
+                    cv.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 3)
+                    cv.putText(image, f'{dataset["categories"][ic]["name"]}, {pred[pred_idx][4]:.2f}', (x1 ,y1), cv.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3, cv.LINE_AA)
 
-        for i, pred_matches in enumerate(matches_matrix):
-            num_matches = pred_matches.sum()
-            if num_matches == 0:
-                false_positives += 1
+                elif num_matches == 0:
+                    false_negatives += 1
+                    
+                    # Draw false negatives as blue
+                    x1, y1, width, height = gt_bboxes[i]
+                    x2 = x1 + width
+                    y2 = y1 + height
+                    x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+                    cv.rectangle(image, (x1, y1), (x2, y2), (255, 0, 0), 3)
+                    cv.putText(image, f'{dataset["categories"][ic]["name"]}', (x1 ,y1), cv.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 0), 3, cv.LINE_AA)
 
-                # Draw false negatives as red
-                x1, y1, x2, y2 = predictions[i][:4]
-                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-                cv.rectangle(image, (x1, y1), (x2, y2), (0, 0, 255), 1)
-
+            for i, pred_matches in enumerate(matches_matrix):
+                num_matches = pred_matches.sum()
+                if num_matches == 0:                    
+                    # Draw false negatives as red
+                    x1, y1, x2, y2 = pred[i][:4]
+                    x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+                    false_positives += 1
+                    cv.rectangle(image, (x1, y1), (x2, y2), (0, 0, 255), 3)
+                    cv.putText(image, f'{dataset["categories"][ic]["name"]}, {pred[i][4]:.2f}', (x1 ,y1), cv.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3, cv.LINE_AA)
+                    
         draw_text(image, true_positives, false_positives, false_negatives)
-
         all_true_positives += true_positives
         all_false_positives += false_positives
         all_false_negatives += false_negatives
@@ -176,7 +182,7 @@ if __name__ == "__main__":
         viz_path = os.path.join(args.viz_dir, f"{image_info['file_name']}.jpg")
         cv.imwrite(viz_path, image)
 
-        print(viz_path, true_positives, false_positives, false_negatives)
+#        print(viz_path, true_positives, false_positives, false_negatives)
 
     print(f"True positives: {all_true_positives}")
     print(f"False positives: {all_false_positives}")

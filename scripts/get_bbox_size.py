c2c8d397817d3cbc084243eba09003d7aa050628
Théo Megy <theo.megy@logiroad.fr>
2022-10-28 16:16:49 +0200

    get bboxes size by class

--- scripts/get_bbox_size.py
+++ scripts/get_bbox_size.py
@@ -3,6 +3,8 @@ import argparse
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+mpl.use('Agg')
 
 
 def main(args):
@@ -17,43 +19,47 @@ def main(args):
     with open(args.infile, 'r') as f:
         in_dict = json.load(f)
 
-    size_list = []
-    n_small = 0
-    n_medium = 0
-    n_large = 0
+    n_class = len(in_dict['categories'])
+        
+    size_list = [[] for _ in range(n_class)]
+    
+    n_small = [0 for _ in range(n_class)]
+    n_medium = [0 for _ in range(n_class)]
+    n_large = [0 for _ in range(n_class)]
     for ann in in_dict['annotations']:
-        size_list.append( (ann['bbox'][2]+ann['bbox'][3])/2 )
-        #area = ann['area']
+        cat_id = ann['category_id']
+        size_list[cat_id].append( (ann['bbox'][2]+ann['bbox'][3])/2 )
         area = ann['bbox'][2]*ann['bbox'][3]
-        #print(area, ann['bbox'][2]*ann['bbox'][3])
         if area < 32*32:
-            n_small += 1
+            n_small[cat_id] += 1
         elif area > 96*96:
-            n_large += 1
+            n_large[cat_id] += 1
         else:
-            n_medium += 1
+            n_medium[cat_id] += 1
 
-    print('')
-    print(f'small objects: {n_small}')
-    print(f'medium objects: {n_medium}')
-    print(f'large objects: {n_large}')
-    print('')
+    for cat in in_dict['categories']:
+        print('')
+        print(cat['name'])
+        print(f'small objects: {n_small[cat["id"]]}')
+        print(f'medium objects: {n_medium[cat["id"]]}')
+        print(f'large objects: {n_large[cat["id"]]}')
+        print('')
 
-    size_list = np.array(size_list)
-    label = f'mean={size_list.mean():.1f}\nstd={size_list.std():.1f}'
-    print('(height+width)/2')
-    print(label)
-    print('')
+        size_list_cat = np.array(size_list[cat["id"]])
+        label = f'mean={size_list_cat.mean():.1f}\nstd={size_list_cat.std():.1f}'
+        print('(height+width)/2')
+        print(label)
+        print('')
 
-    fig, ax1 = plt.subplots()
-    ax1.set_ylabel('# bboxes')
-    ax1.set_xlabel('(height+width)/2')
-    ax1.hist(size_list, 25, label=label)
-    ax1.legend()
+        fig, ax1 = plt.subplots()
+        ax1.set_ylabel('# bboxes')
+        ax1.set_xlabel('(height+width)/2')
+        ax1.hist(size_list_cat, 25, label=label)
+        ax1.legend()
 
-    name = args.infile.split('/')[-1].replace('.json', '')
-    fig.savefig(f'plots/preprocessing/{name}_bbox_size.pdf')
-    print(f'Saved plots/preprocessing/{name}_bbox_size.pdf\n')
+        name = args.infile.split('/')[-1].replace('.json', '')
+        fig.savefig(f'plots/preprocessing/{name}_{cat["name"]}_bbox_size.pdf')
+        print(f'Saved plots/preprocessing/{name}_{cat["name"]}_bbox_size.pdf\n')
 
 
 if __name__ == '__main__':

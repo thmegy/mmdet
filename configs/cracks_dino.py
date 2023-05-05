d55a7ad1dcc339c78a8f639e47578d4d88deb33e
Théo Megy <theo.megy@logiroad.fr>
2023-05-05 16:51:28 +0200

    keep only mmdet-v3-compatible configs

--- configs/cracks_dino.py
+++ configs/cracks_dino.py
@@ -1,4 +1,4 @@
-_base_ = '../mmdetection/configs/dyhead/atss_swin-l-p4-w12_fpn_dyhead_ms-2x_coco.py'
+_base_ = '../mmdetection/configs/dino/dino-5scale_swin-l_8xb2-12e_coco.py'
 
 model = dict(
     bbox_head=dict(
@@ -7,19 +7,27 @@ model = dict(
 )
 
 train_pipeline = [
-    dict(type='LoadImageFromFile', backend_args={{_base_.backend_args}}),
+    dict(type='LoadImageFromFile', backend_args=None),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(
-        type='RandomResize',
-        scale=[(1000, 480), (1000, 600)],
-        keep_ratio=True,
-        backend='pillow'),
     dict(type='RandomFlip', prob=0.5),
+    dict(
+        type='RandomChoice',
+        transforms=[
+            [{
+            'type':
+            'RandomChoiceResize',
+            'scales': [(480, 1000), (512, 1000), (544, 1000), (576, 1000),
+                       (608, 1000), (640, 1000), (672, 1000), (700, 1000)],
+            'keep_ratio':
+            True
+            }],
+        ]
+    ),
     dict(type='PackDetInputs')
 ]
 test_pipeline = [
-    dict(type='LoadImageFromFile', backend_args={{_base_.backend_args}}),
-    dict(type='Resize', scale=(1000, 600), keep_ratio=True, backend='pillow'),
+    dict(type='LoadImageFromFile', backend_args=None),
+    dict(type='Resize', scale=(1000, 700), keep_ratio=True),
     dict(type='LoadAnnotations', with_bbox=True),
     dict(
         type='PackDetInputs',
@@ -37,17 +45,13 @@ train_dataloader = dict(
     batch_size=1,
     num_workers=2,
     dataset=dict(
-        type='RepeatDataset',
-        times=2,
-        dataset=dict(
-            type=dataset_type,
-            data_root=data_root,
-            ann_file='cracks_train.json',
-            data_prefix=dict(img='/home/finn/DATASET/ai4cracks-dataset/images/train/'),
-            filter_cfg=dict(filter_empty_gt=False),
-            metainfo=dict(classes=classes),
-            pipeline=train_pipeline
-        )
+        type=dataset_type,
+        data_root=data_root,
+        ann_file='cracks_train.json',
+        data_prefix=dict(img='/home/finn/DATASET/ai4cracks-dataset/images/train/'),
+        filter_cfg=dict(filter_empty_gt=False),
+        metainfo=dict(classes=classes),
+        pipeline=train_pipeline
     )
 )
 
@@ -78,10 +82,15 @@ val_evaluator = dict(
 test_evaluator = val_evaluator
 
 # training parameters
-train_cfg = dict(
-    val_interval=2  # Interval for validation, check the performance every 2
+optim_wrapper = dict(
+    optimizer=dict(lr=0.0001 * 1 / 16)
 )
-load_from='pretrained_checkpoints/atss_swin-l-p4-w12_fpn_dyhead_mstrain_2x_coco_20220509_100315-bc5b6516.pth'
+train_cfg = dict(
+    val_interval=1  # Interval for validation, check the performance every 2
+)
+
+load_from='pretrained_checkpoints/dino-5scale_swin-l_8xb2-12e_coco_20230228_072924-a654145f.pth'
+
 
 default_hooks = dict(
     checkpoint=dict(
@@ -90,7 +99,7 @@ default_hooks = dict(
     ),
         logger=dict(
             type='LoggerHook',
-            interval=50
+            interval=1500
     )
 )
 
